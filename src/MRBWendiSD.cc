27770fdd4b5b32ff58f2874896172baae78f9abd
Htoo Tay Za <155787255+HtooTayZa@users.noreply.github.com>
2026-02-26 20:37:16 +0630

    Update Doc MRBWendiSD.cc

--- src/MRBWendiSD.cc
+++ src/MRBWendiSD.cc
@@ -11,7 +11,11 @@ void MRBWendiSD::Initialize(G4HCofThisEvent*) {}
 
 G4bool MRBWendiSD::ProcessHits(G4Step* step, G4TouchableHistory*)
 {
+    // Filter specifically for neutrons entering the WENDI detector
     if (step->GetTrack()->GetDefinition() == G4Neutron::NeutronDefinition()) {
+        
+        // We only want to count the neutron once when it enters the detector volume (crosses the boundary)
+        // to prevent overcounting from multiple steps taken by the same neutron inside the gas.
         if (step->GetPreStepPoint()->GetStepStatus() == fGeomBoundary) {
             fEventAction->AddWendiNeutron();
         }
