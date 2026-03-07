b047a2bec29e70cb9cac344c888325d2cd65a86f
Htoo Tay Za <155787255+HtooTayZa@users.noreply.github.com>
2026-03-07 10:26:45 +0630

    Update MRBEventAction.hh

--- include/MRBEventAction.hh
+++ include/MRBEventAction.hh
@@ -15,18 +15,30 @@ class MRBEventAction : public G4UserEventAction
     virtual void BeginOfEventAction(const G4Event* event);
     virtual void EndOfEventAction(const G4Event* event);
 
+    // --- Existing Functionality: Data Collection ---
     void AddHitDWC0() { fHitCountDWC0++; }
     void AddHitDWC1() { fHitCountDWC1++; }
     void AddHitDWC2() { fHitCountDWC2++; }
     void AddCaloEnergy(G4double edep) { fTotalCaloEnergy += edep; }
     void AddWendiNeutron() { fHitCountWendi++; }
 
+    // --- New Functionality: Hardware Trigger Logic ---
+    void RegisterSinHit(G4double time);
+    void RegisterVetoHit(G4double time);
+    void SetCalHit(G4bool hit) { fCalHit = hit; }
+
   private:
+    // --- Existing Variables ---
     G4int fHitCountDWC0;
     G4int fHitCountDWC1;
     G4int fHitCountDWC2;
     G4double fTotalCaloEnergy;
     G4int fHitCountWendi; 
+
+    // --- New Variables ---
+    G4double fSinTime;
+    G4double fVetoTime;
+    G4bool   fCalHit;
 };
 
 #endif
