e6964c5f485c896d1b50aaedc92d4ae043e65610
Htoo Tay Za <155787255+HtooTayZa@users.noreply.github.com>
2026-03-07 10:46:13 +0630

    Update MRBWendiSD.cc

--- src/MRBWendiSD.cc
+++ src/MRBWendiSD.cc
@@ -1,25 +1,40 @@
 #include "MRBWendiSD.hh"
+#include "G4HCofThisEvent.hh"
 #include "G4Step.hh"
-#include "G4Track.hh"
+#include "G4SDManager.hh"
 #include "G4Neutron.hh"
 
-MRBWendiSD::MRBWendiSD(const G4String& name, MRBEventAction* eventAction)
-: G4VSensitiveDetector(name), fEventAction(eventAction) {}
+MRBWendiSD::MRBWendiSD(const G4String& name)
+: G4VSensitiveDetector(name), fHitsCollection(nullptr) 
+{
+    // Register the hits collection name with Geant4
+    collectionName.insert("WendiHitsCollection");
+}
 
 MRBWendiSD::~MRBWendiSD() {}
-void MRBWendiSD::Initialize(G4HCofThisEvent*) {}
 
-G4bool MRBWendiSD::ProcessHits(G4Step* step, G4TouchableHistory*)
+void MRBWendiSD::Initialize(G4HCofThisEvent* hce) 
+{
+    // Create the hits collection at the start of each event
+    fHitsCollection = new MRBWendiHitsCollection(SensitiveDetectorName, collectionName[0]);
+    
+    // Add it to the event's overall hit collection map
+    G4int hcID = G4SDManager::GetSDMpointer()->GetCollectionID(collectionName[0]);
+    hce->AddHitsCollection(hcID, fHitsCollection);
+}
+
+G4bool MRBWendiSD::ProcessHits(G4Step* step, G4TouchableHistory*) 
 {
     // Filter specifically for neutrons entering the WENDI detector
     if (step->GetTrack()->GetDefinition() == G4Neutron::NeutronDefinition()) {
-        
-        // We only want to count the neutron once when it enters the detector volume (crosses the boundary)
-        // to prevent overcounting from multiple steps taken by the same neutron inside the gas.
         if (step->GetPreStepPoint()->GetStepStatus() == fGeomBoundary) {
-            fEventAction->AddWendiNeutron();
+            
+            // Create a new hit and add it to the collection
+            MRBWendiHit* newHit = new MRBWendiHit();
+            fHitsCollection->insert(newHit);
         }
     }
     return true;
 }
+
 void MRBWendiSD::EndOfEvent(G4HCofThisEvent*) {}
