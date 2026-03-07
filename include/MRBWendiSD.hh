28b27533a33c317eccbaa57cb849132cc21d9cfb
Htoo Tay Za <155787255+HtooTayZa@users.noreply.github.com>
2026-03-07 10:44:10 +0630

    Update MRBWendiSD.hh

--- include/MRBWendiSD.hh
+++ include/MRBWendiSD.hh
@@ -1,21 +1,23 @@
 #ifndef MRBWendiSD_h
 #define MRBWendiSD_h 1
+
 #include "G4VSensitiveDetector.hh"
-#include "MRBEventAction.hh"
+#include "MRBWendiHit.hh" // Include the new hit class
+
 class G4Step;
 class G4HCofThisEvent;
 
 class MRBWendiSD : public G4VSensitiveDetector
 {
   public:
-    MRBWendiSD(const G4String& name, MRBEventAction* eventAction);
+    MRBWendiSD(const G4String& name); // Removed eventAction pointer
     virtual ~MRBWendiSD();
 
-    virtual void Initialize(G4HCofThisEvent* hitCollection);
-    virtual G4bool ProcessHits(G4Step* step, G4TouchableHistory* history);
-    virtual void EndOfEvent(G4HCofThisEvent* hitCollection);
+    virtual void Initialize(G4HCofThisEvent* hitCollection) override;
+    virtual G4bool ProcessHits(G4Step* step, G4TouchableHistory* history) override;
+    virtual void EndOfEvent(G4HCofThisEvent* hitCollection) override;
 
   private:
-    MRBEventAction* fEventAction;
+    MRBWendiHitsCollection* fHitsCollection;
 };
 #endif
